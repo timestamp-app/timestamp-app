fc2078a6f4a8982fe4fe973481aef025a5aa9c75
tom reilly <treilly94@live.com>
2020-04-27 22:37:47 +0100

    moved formatting stuff into its own function

--- datainput/__init__.py
+++ datainput/__init__.py
@@ -7,6 +7,17 @@ from uuid import uuid4
 from datetime import datetime
 
 
+def format_input(data):
+    # Add azure info
+    data["PartitionKey"] = str(datetime.today().year)
+    data["RowKey"] = str(uuid4())
+
+    # Change the datetime format from IFTTT to ISO 
+    ifttt_time_fmt = "%B %d, %Y at %I:%M%p"
+    data["time"] = datetime.strptime(data["time"], ifttt_time_fmt).isoformat()
+
+    return data
+
 def main(req: func.HttpRequest, storageOut: func.Out[str]) -> func.HttpResponse:
     logging.info('Function processed a request.')
 
@@ -17,13 +28,15 @@ def main(req: func.HttpRequest, storageOut: func.Out[str]) -> func.HttpResponse:
         logging.info('Request Failed: No JSON input supplied.')
         return func.HttpResponse("Requires JSON input", status_code=400)
 
+    # Format input
+    try:
+        req_body = format_input(req_body)
+    except:
+        logging.info('Request Failed: Could not format input.')
+        return func.HttpResponse("Could not format input", status_code=500)
+
     # Write input
     try:
-        req_body["PartitionKey"] = str(datetime.today().year)
-        req_body["RowKey"] = str(uuid4())
-        # Change the datetime format from IFTTT to ISO 
-        req_body["time"] = datetime.strptime(req_body["time"], "%B %d, %Y at %I:%M%p").isoformat()
-
         storageOut.set(json.dumps(req_body))
     except:
         logging.error('Request Failed: Could not write to storage.')
