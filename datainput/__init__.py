e83f3c355a6a79a1c5d25f8dc51fdd0d38683310
tom reilly <treilly94@live.com>
2020-05-02 17:18:58 +0100

    Added step to make it trigger the html generator

--- datainput/__init__.py
+++ datainput/__init__.py
@@ -1,3 +1,5 @@
+import os
+import requests
 import logging
 import json
 
@@ -44,5 +46,17 @@ def main(req: func.HttpRequest, storageOut: func.Out[str]) -> func.HttpResponse:
     except:
         logging.error('Request Failed: Could not write to storage.')
         return func.HttpResponse("Could not write to storage", status_code=500)
+
+    # Trigger HTML Generator
+    try:
+        gen_url = 'https://' + os.getenv("WEBSITE_HOSTNAME") + '/api/htmlgenerator'
+        payload = {'code': os.getenv("HTMLGENERATOR_KEY")}
+        r = requests.get(gen_url, params=payload)
+    except requests.exceptions.HTTPError as e:
+        logging.error("Invalid response code form html generator: " + e.response)
+        return func.HttpResponse("Invalid response code form html generator: " + e.response, status_code=500)
+    except:
+        logging.error("Couldnt trigger html generator")
+        return func.HttpResponse("Couldnt trigger html generator", status_code=500)
 
     return func.HttpResponse("Success")
