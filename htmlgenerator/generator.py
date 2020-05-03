0e84c569da1eadfc0280e047557bcc719a0cd2a5
tom reilly <treilly94@live.com>
2020-05-03 16:40:49 +0100

    added a extra graph

--- htmlgenerator/generator.py
+++ htmlgenerator/generator.py
@@ -2,33 +2,61 @@ from plotly.offline import plot
 from jinja2 import Environment, FileSystemLoader
 import plotly.graph_objs as go
 import pandas as pd
+import copy
 
 class generator():
-
-    date_plot = ""
+    
+    df = ""
+    date_line_html = ""
+    month_line_html = ""
 
     def __init__(self, data):
-        df = pd.DataFrame(data)
-        df['time'] = pd.to_datetime(df['time'], format="%Y/%m/%d %H:%M")
-
-        self.df = df
+        self.df = pd.DataFrame(data)
+        self.df['time'] = pd.to_datetime(
+            self.df['time'],
+            format="%Y/%m/%d %H:%M"
+            )
 
     def make_plots(self):
-        self.date_plot()
+        self.date_line_plot()
+        self.month_line_plot()
 
     def make_html(self, working_directory):
         """Creates a html file from a j2 template"""
         file_loader = FileSystemLoader(working_directory + "/templates")
         env = Environment(loader=file_loader)
         template = env.get_template("template.html.j2")
-        return template.render(plot=self.date_plot)
+        return template.render(
+            date_line_html=self.date_line_html,
+            month_line_html=self.month_line_html
+            )
         
+    def _line_plot(self, data):
+        fig = go.Figure(data=go.Scatter(x=data.index, y=data.values))
+        fig.update_xaxes(
+            rangeslider_visible=True,
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1, label="1m", step="month", stepmode="backward"),
+                    dict(count=6, label="6m", step="month", stepmode="backward"),
+                    dict(count=1, label="YTD", step="year", stepmode="todate"),
+                    dict(count=1, label="1y", step="year", stepmode="backward"),
+                    dict(step="all")
+                ])
+            )
+        )
+        return plot(fig, output_type="div")
 
-    def date_plot(self):
-        df = self.df
-        df['time'] = df['time'].dt.date
-        counts = df['time'].value_counts()
+    def date_line_plot(self):
+        data = copy.deepcopy(self.df)
+        data['time'] = data['time'].dt.date
+        counts = data['time'].value_counts().sort_index()
 
-        bar = go.Bar(x=counts.index, y=counts.values)
+        self.date_line_html =  self._line_plot(counts)
 
-        self.date_plot =  plot([bar], output_type="div")
+    def month_line_plot(self):
+        data = copy.deepcopy(self.df)
+        data['time'] = data['time'].dt.month
+        counts = data['time'].value_counts().sort_index()
+
+        self.month_line_html =  self._line_plot(counts)
