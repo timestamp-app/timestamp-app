88dbc11329a947c3d94bd4205aad7130a5d3296c
tom reilly <treilly94@live.com>
2020-05-14 15:09:58 +0100

    Added push notifacations

--- datainput/__init__.py
+++ datainput/__init__.py
@@ -23,6 +23,26 @@ def format_input(data):
 
     return data
 
+def push_notification(code, message):
+    try:
+        wirepusher_url = 'https://wirepusher.com/send'
+        payload = {
+            'id': os.getenv("WIREPUSHER_ID"),
+            'title': 'Timestamp ' + code,
+            'message': message,
+            'type': code
+            } 
+        requests.get(wirepusher_url, params=payload)
+    except requests.exceptions.HTTPError as e:
+        logging.error("Request Failed: Invalid response code from wirepusher: " + e.response)
+    except:
+        logging.error("Request Failed: Couldnt trigger wirepusher")
+
+def handle_error(message, code=500):
+    logging.error(message)
+    push_notification("Error", message)
+    return func.HttpResponse(message, status_code=code)
+
 def main(req: func.HttpRequest, storageOut: func.Out[str]) -> func.HttpResponse:
     logging.info('Function processed a request.')
 
@@ -37,26 +57,23 @@ def main(req: func.HttpRequest, storageOut: func.Out[str]) -> func.HttpResponse:
     try:
         req_body = format_input(req_body)
     except:
-        logging.info('Request Failed: Could not format input.')
-        return func.HttpResponse("Could not format input", status_code=500)
+        return handle_error('Request Failed: Could not format input.')
 
     # Write input
     try:
         storageOut.set(json.dumps(req_body))
     except:
-        logging.error('Request Failed: Could not write to storage.')
-        return func.HttpResponse("Could not write to storage", status_code=500)
+        return handle_error('Request Failed: Could not write to storage.')
 
     # Trigger HTML Generator
     try:
         gen_url = 'https://' + os.getenv("WEBSITE_HOSTNAME") + '/api/htmlgenerator'
         payload = {'code': os.getenv("HTMLGENERATOR_KEY")}
-        r = requests.get(gen_url, params=payload)
+        requests.get(gen_url, params=payload)
     except requests.exceptions.HTTPError as e:
-        logging.error("Invalid response code form html generator: " + e.response)
-        return func.HttpResponse("Invalid response code form html generator: " + e.response, status_code=500)
+        return handle_error("Request Failed: Invalid response code from html generator: " + e.response)
     except:
-        logging.error("Couldnt trigger html generator")
-        return func.HttpResponse("Couldnt trigger html generator", status_code=500)
+        return handle_error("Request Failed: Couldnt trigger html generator")
 
+    push_notification("Success", "Record added at: " + str(datetime.now()))
     return func.HttpResponse("Success")
