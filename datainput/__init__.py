75d7c1331e53c0cf2c7211296bba87f2e4ee7b7e
treilly94 <treilly94@live.com>
2020-05-22 18:50:16 +0100

    Htmlgenerator timer (#11)
    * removed extra brackets
    
    * Added timer trigger
    
    * removed html generator trigger from data_input
    
    * removed html generator http trigger

--- datainput/__init__.py
+++ datainput/__init__.py
@@ -44,15 +44,5 @@ def main(req: func.HttpRequest, storageout: func.Out[str]) -> func.HttpResponse:
     except:
         return handle_error('Request Failed: Could not write to storage.')
 
-    # Trigger HTML Generator
-    try:
-        gen_url = 'https://' + os.getenv("WEBSITE_HOSTNAME") + '/api/htmlgenerator'
-        payload = {'code': os.getenv("HTMLGENERATOR_KEY")}
-        requests.get(gen_url, params=payload)
-    except requests.exceptions.HTTPError as err:
-        return handle_error("Request Failed: Invalid response code from html generator: " + err)
-    except:
-        return handle_error("Request Failed: Couldnt trigger html generator")
-
     push_notification("Success", "Record added at: " + str(datetime.now()))
     return func.HttpResponse("Success")
