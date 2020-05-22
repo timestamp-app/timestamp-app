4f845b7d1d3bbccaf879a3df4a26e97d50bb2ce4
tom reilly <treilly94@live.com>
2020-05-22 14:27:34 +0100

    Created DataWrangler class

--- datainput/__init__.py
+++ datainput/__init__.py
@@ -1,37 +1,16 @@
 """
 This module is responsible for ingesting data and writing it to storage
 """
-import json
 import logging
 import os
 from datetime import datetime
-from uuid import uuid4
 
 import azure.functions as func
 import requests
 
+from datainput.data_wrangler import DataWrangler
 from helpers.error_handling import handle_error
 from helpers.notifications import push_notification
-
-
-def format_input(data):
-    """
-    This function formats the input data to be written into th table
-    :param data:
-    :return:
-    """
-    # Add azure info
-    data["PartitionKey"] = str(datetime.today().year)
-    data["RowKey"] = str(uuid4())
-
-    # Change the datetime format from IFTTT
-    # Was going to use ISO but azure tables detects that format and changes it
-    ifttt_time_fmt = "%B %d, %Y at %I:%M%p"
-    prefered_time_fmt = "%Y/%m/%d %H:%M"
-    datetime_obj = datetime.strptime(data["time"], ifttt_time_fmt)
-    data["time"] = datetime_obj.strftime(prefered_time_fmt)
-
-    return data
 
 
 # pylint: disable=E1136,W0702
@@ -47,19 +26,21 @@ def main(req: func.HttpRequest, storageout: func.Out[str]) -> func.HttpResponse:
     # Get Input
     try:
         req_body = req.get_json()
+        wrangler = DataWrangler(req_body)
     except:
         logging.info('Request Failed: No JSON input supplied.')
         return func.HttpResponse("Requires JSON input", status_code=400)
 
     # Format input
     try:
-        req_body = format_input(req_body)
+        wrangler.add_key_values()
+        wrangler.format_time()
     except:
         return handle_error('Request Failed: Could not format input.')
 
     # Write input
     try:
-        storageout.set(json.dumps(req_body))
+        wrangler.write_to_table(table=storageout)
     except:
         return handle_error('Request Failed: Could not write to storage.')
 
