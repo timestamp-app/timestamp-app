cc40c68d0d12939a28de55207c74f15dc7c65193
tom reilly <treilly94@live.com>
2020-05-22 15:06:47 +0100

    created add_key_values test

---
+++ test/test_data_wrangler.py
@@ -0,0 +1,38 @@
+import json
+from unittest import TestCase
+from freezegun import freeze_time
+from mock import patch
+
+from datainput import DataWrangler
+
+
+class TestDataWrangler(TestCase):
+
+    def setUp(self) -> None:
+        with open(file='test/mock_input.json') as f:
+            raw_input = f.read()
+
+        input_data = json.loads(raw_input)
+        self.wrangler = DataWrangler(data=input_data)
+
+    @patch('datainput.data_wrangler.uuid4', return_value='1f83f6a2-3841-45da-8129-98de28ce7b74')
+    @freeze_time("2012-01-01")
+    def test_add_key_values(self, uuid4):
+
+        expected_data = {
+            'PartitionKey': '2012',
+            'RowKey': '1f83f6a2-3841-45da-8129-98de28ce7b74',
+            'lat': '57.513195',
+            'long': '3.8307557',
+            'time': 'April 27, 2020 at 09:28PM'
+        }
+
+        self.wrangler.add_key_values()
+
+        self.assertEqual(self.wrangler.data, expected_data)
+
+    def test_format_time(self):
+        self.fail()
+
+    def test_write_to_table(self):
+        self.fail()
--- test/test_datainput.py
+++ test/test_datainput.py
@@ -1,11 +1,8 @@
 import unittest
-import json
 
 import azure.functions as func
 
-from datainput import main, format_input
-from freezegun import freeze_time
-from mock import patch
+from datainput import main
 
 
 class TestMain(unittest.TestCase):
@@ -23,29 +20,3 @@ class TestMain(unittest.TestCase):
 
         # Assert
         self.assertEqual(resp.get_body(), b'Requires JSON input')
-
-
-class TestFormatInput(unittest.TestCase):
-
-    @patch('datainput.uuid4', return_value='1f83f6a2-3841-45da-8129-98de28ce7b74')
-    @freeze_time("2012-01-01")
-    def test_ifttt_format(self, uuid4):
-        # Input
-        with open(file='test/mock_input.json') as f:
-            raw_input = f.read()
-        
-        input_data = json.loads(raw_input)
-
-        # Expected
-        expected_data = {
-            'PartitionKey': '2012',
-            'RowKey': '1f83f6a2-3841-45da-8129-98de28ce7b74',
-            'lat': '57.513195',
-            'long': '3.8307557',
-            'time': '2020/04/27 21:28'
-        }
-        
-        resp = format_input(input_data)
-
-        self.assertEqual(resp, expected_data)
-
