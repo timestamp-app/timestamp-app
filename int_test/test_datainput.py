3db5a7e38fcf622d96dbe1bbdb04cead61dda00b
tom reilly <treilly94@live.com>
2020-05-26 16:35:06 +0100

    Allowed for local int testing

--- int_test/test_datainput.py
+++ int_test/test_datainput.py
@@ -7,7 +7,14 @@ import requests
 class TestDataInput(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.url = "https://devduck.azurewebsites.net/api/datainput"
+        if os.getenv("ENV") == 'dev':
+            root_url = "https://devduck.azurewebsites.net"
+        elif os.getenv("ENV") == 'prod':
+            root_url = "https://wolfduck.azurewebsites.net"
+        else:
+            root_url = 'http://localhost:7071'
+
+        self.url = root_url + "/api/datainput"
         self.params = {"code": os.getenv("FUNCTION_KEY")}
 
     def test_healthcheck(self):
