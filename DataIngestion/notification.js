41eb73503b16c757c79364d65f9995620c7b9a03
tom reilly <treilly94@live.com>
2020-10-01 15:09:26 +0100

    added notify function

---
+++ DataIngestion/notification.js
@@ -0,0 +1,11 @@
+module.exports = {
+    notify: function(context, status, message) {
+        const axios = require('axios')
+        const id = process.env.WIREPUSHER_ID
+        const title = `Timestamp ${status}`
+
+        axios.post(`https://wirepusher.com/send?id=${id}&title=${title}&message=${message}&type=${status}`)
+
+        context.log('Sent Notification');
+    }
+}
