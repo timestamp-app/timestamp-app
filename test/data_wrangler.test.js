33520a46bad4e54a51961bb22470bb736c87bd9c
tom reilly <treilly94@live.com>
2020-10-02 14:20:47 +0100

    added add_key_values test

--- test/data_wrangler.test.js
+++ test/data_wrangler.test.js
@@ -1,15 +1,37 @@
-const { TestScheduler } = require('jest')
-const input_data = require('./mock_input.json')
-const wrangler = require('../DataIngestion/data_wrangler.js')
+const fs = require('fs');
 
-test('formatting the timestamp', () => {
-    const expected_data = {
-        time: '2020/04/27 21:28',
-        lat: '57.513195',
-        long: '3.8307557'
-    }
+describe('wrangler tests', () => {
+    let wrangler = require('../DataIngestion/data_wrangler.js')
+    let input_data
+    
+    beforeEach(() => {
+        let rawdata = fs.readFileSync('./test/mock_input.json')
+        input_data = JSON.parse(rawdata)
+    })
+    
+    test('add key values', () => {
+        let expected_data = {
+            PartitionKey: '2012',
+            RowKey: '1f83f6a2-3841-45da-8129-98de28ce7b74',
+            time: 'April 27, 2020 at 09:28PM',
+            lat: '57.513195',
+            long: '3.8307557'
+        }
+    
+        wrangler.add_key_values(input_data)
+    
+        expect(input_data).toBe(expected_data);
+    })
 
-    wrangler.format_time(input_data)
-
-    expect(input_data).toBe(expected_data);
-});
+    test('formatting the timestamp', () => {
+        let expected_data = {
+            time: '2020/04/27 21:28',
+            lat: '57.513195',
+            long: '3.8307557'
+        }
+    
+        wrangler.format_time(input_data)
+    
+        expect(input_data).toBe(expected_data);
+    })
+})
