e487f1bfc9c792b343b6c3e4bbb59bc10aadc9dc
tom <treilly94@live.com>
2021-01-04 22:49:14 +0000

    added terraform

---
+++ terraform/main.tf
@@ -0,0 +1,42 @@
+provider "azurerm" {
+  version = "=2.41.0"
+  features {}
+}
+
+locals {
+    name = "timestampcollector"
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = local.name
+  location = "uksouth"
+}
+
+resource "azurerm_storage_account" "this" {
+  name                     = local.name
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "this" {
+  name                = local.name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  kind                = "FunctionApp"
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "this" {
+  name                       = local.name
+  location                   = azurerm_resource_group.this.location
+  resource_group_name        = azurerm_resource_group.this.name
+  app_service_plan_id        = azurerm_app_service_plan.this.id
+  storage_account_name       = azurerm_storage_account.this.name
+  storage_account_access_key = azurerm_storage_account.this.primary_access_key
+}
