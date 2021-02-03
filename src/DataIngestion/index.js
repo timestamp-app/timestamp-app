5e3a5d0af53f74c6abd605346ab239727629e3a0
tom <treilly94@live.com>
2021-02-03 17:03:22 +0000

    removed healthcheck

--- src/DataIngestion/index.js
+++ src/DataIngestion/index.js
@@ -2,38 +2,23 @@ module.exports = async function (context, req) {
     const notification = require('./notification.js')
     const wrangler = require('./data_wrangler.js')
 
-    if (req.method == 'GET' && req.params.health == 'health') {
-        // Healthcheck
-        context.log.verbose('Healthcheck request recieved',);
+    context.log.info('POST request received');
 
-        const pjson = require('../package.json');
-        const response = JSON.stringify({
-            status: 'Healthy',
-            version: pjson.version
-        })
-        context.res = {
-            body: response
-        };
-    } else if (req.method == 'POST') {
-        // Data Input
-        context.log.info('POST request recieved');
+    wrangler.add_key_values(req.body)
+    wrangler.format_time(req.body)
 
-        wrangler.add_key_values(req.body)
-        wrangler.format_time(req.body)
+    notification.notify(
+        context,
+        'Success',
+        `Record added at: ${new Date().toUTCString()}`
+        )
 
-        notification.notify(
-            context,
-            'Success',
-            `Record added at: ${new Date().toUTCString()}`
-            )
-
-        const response = JSON.stringify({
-            status: 'Success',
-            record: req.body
-        })
-        context.res = {
-            body: response
-        };
-        context.log.info('POST request success');
-    }
+    const response = JSON.stringify({
+        status: 'Success',
+        record: req.body
+    })
+    context.res = {
+        body: response
+    };
+    context.log.info('POST request success');
 }
