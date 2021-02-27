2f6927e3a9ef1113edef0e9e0d0d5ebbe262de1d
tom <treilly94@live.com>
2021-02-27 20:57:10 +0000

    changed log

--- Output/index.ts
+++ Output/index.ts
@@ -1,7 +1,7 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
+    context.log('Output Triggered');
     const responseMessage = context.bindings.tableIn
 
     context.res = {
