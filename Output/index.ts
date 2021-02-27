ebcca7d6ef10459a5020f6515ca06769ee5ec827
tom <treilly94@live.com>
2021-02-27 20:09:00 +0000

    fixed error with table read

--- Output/index.ts
+++ Output/index.ts
@@ -2,7 +2,7 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const responseMessage = context.bindings.tableIn.pull()
+    const responseMessage = context.bindings.tableIn
 
     context.res = {
         body: responseMessage
